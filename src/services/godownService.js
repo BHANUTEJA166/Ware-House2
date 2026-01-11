677edde88af7f8f0cd79d1017943c772d1192d8b
BHANUTEJA166 <bhanutejak116@gmail.com>
2026-01-11 15:03:17 +0530

    godownservices

--- src/services/godownService.js
+++ src/services/godownService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "/data/warehouses.json";
 
 export const getGodownsByWarehouse = async (warehouseId) => {
   try {
